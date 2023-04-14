5c34a07860ed6074386a9128855b29778394adec
JKilpatrick1 <112705716+JKilpatrick1@users.noreply.github.com>
2023-04-14 11:40:44 -0500

    Update GroupProject.sql

--- GroupProject.sql
+++ GroupProject.sql
@@ -1,37 +1,39 @@
-CREATE TABLE Contributor(
+CREATE TABLE Contributor (
   ContributorID INT(6) NOT NULL AUTO_INCREMENT,
-  Name CHAR(30) NOT NULL,
-  Role CHAR(20) NOT NULL,
+  Name VARCHAR(30) NOT NULL,
   PRIMARY KEY (ContributorID)
 ); 
 
 CREATE TABLE Song (
   SongID INT(6) NOT NULL AUTO_INCREMENT,
-  Title CHAR(15) NOT NULL,
-  File CHAR(10) NOT NULL,
-  Artist CHAR(30) NOT NULL,
+  Title VARCHAR(30) NOT NULL,
+  FileURL VARCHAR(500) NOT NULL,
   PRIMARY KEY (SongID)
 ); 
 
 CREATE TABLE User (
   UserID INT(6) NOT NULL AUTO_INCREMENT,
-  Name CHAR(20) NOT NULL,
+  Name VARCHAR(30) NOT NULL,
   PRIMARY KEY (UserID)
 ); 
 
-CREATE TABLE SongContribution(
-     ContributorID INT(6),
-     SongID INT(6),
-     PRIMARY KEY(ContributorID, SongID),
-     FOREIGN KEY(ContributorID)REFERENCES Contributor(ContributorID),
-     FOREIGN KEY(SongID) REFERENCES Song(SongID)  );
+CREATE TABLE SongContribution (
+  ContributorID INT(6),
+  SongID INT(6),
+  --Role VARCHAR(30) NOT NULL,
+  Role ENUM('artist', 'main artist', 'featured artist', 'writer', 'producer', 'engineer', 'mixer', 'editor', 'composer')
+  PRIMARY KEY(ContributorID, Song ID),
+  FOREIGN KEY(ContributorID) REFERENCES Contributor(ContributorID),
+  FOREIGN KEY(SongID) REFERENCES Song(SongID) 
+);
 
-CREATE TABLE Queue(
+CREATE TABLE Queue (
   UserID INT(6),
   SongID INT(6),
-  Timestamp CHAR(15) NOT NULL,
-  PremiumUser CHAR(3) NOT NULL,
-  AmountPaid DECIMAL(19,2), 
+  processing_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PremiumUser BOOLEAN NOT NULL,
+  AmountPaid DECIMAL(4,2), 
   PRIMARY KEY(UserID, SongID),
   FOREIGN KEY(UserID) REFERENCES User(UserID),
-  FOREIGN KEY(SongID) REFERENCES Song(SongID)  );
+  FOREIGN KEY(SongID) REFERENCES Song(SongID)  
+); 
