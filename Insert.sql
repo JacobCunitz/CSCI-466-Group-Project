c150ae795e912b4f5a84e6595884655b46d7ae51
JKilpatrick1 <112705716+JKilpatrick1@users.noreply.github.com>
2023-04-14 11:48:55 -0500

    Update Insert.sql
    Really generic and we can change later but allows us to start on php

--- Insert.sql
+++ Insert.sql
@@ -1,1 +1,14 @@
+INSERT INTO Contributor (Name) VALUES ('testname1');
+INSERT INTO Contributor (Name) VALUES ('testname2');
 
+INSERT INTO Song (Title, FileURL) VALUES ('Test Song 1', 'http://example.com/test1.mp3');
+INSERT INTO Song (Title, FileURL) VALUES ('Test Song 2', 'http://example.com/test2.mp3');
+
+INSERT INTO User (Name) VALUES ('testname1');
+INSERT INTO User (Name) VALUES ('testname2');
+
+INSERT INTO SongContribution (ContributorID, SongID, Role) VALUES (1, 1, 'artist');
+INSERT INTO SongContribution (ContributorID, SongID, Role) VALUES (2, 1, 'producer');
+
+INSERT INTO Queue (UserID, SongID, PremiumUser, AmountPaid) VALUES (1, 1, TRUE, 9.99);
+INSERT INTO Queue (UserID, SongID, PremiumUser, AmountPaid) VALUES (2, 2, FALSE, 0.00);
