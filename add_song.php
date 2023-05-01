37e5116f0098a6734810be713d5b080adb08ae46
JKilpatrick1 <112705716+JKilpatrick1@users.noreply.github.com>
2023-05-01 12:49:24 -0500

    Update add_song.php
    updated amount paid to be correct

--- add_song.php
+++ add_song.php
@@ -10,13 +10,15 @@ $db_pass = "";
 $db_name = "466GroupProj";
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
+//add song to queue
 if (isset($_GET['add'])) {
     $PremiumUser = $_GET['add'];
+    $AmountPaid = $PremiumUser == 0 ? 0.00 : 1.99;
 
-    $stmt = $conn->prepare("INSERT INTO queue (UserID, SongID, processing_time, PremiumUser, AmountPaid) VALUES (?, ?, NOW(), ?, 1.99)");
-    $stmt->bind_param("iii", $UserID, $SongID, $PremiumUser);
+    $stmt = $conn->prepare("INSERT INTO queue (UserID, SongID, processing_time, PremiumUser, AmountPaid) VALUES (?, ?, NOW(), ?, ?)");
+    $stmt->bind_param("iiid", $UserID, $SongID, $PremiumUser, $AmountPaid);
     $stmt->execute();
-    
+
     header("Location: user.php");
     exit;
 }
